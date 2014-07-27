1d03b4816f30d27e866103b9909256f3bcbd2947
Jared Heinly <jheinly@users.noreply.github.com>
2014-07-26 20:01:23 -0400

    Add MATLAB parse SIFT functionality.
    Added a first version of a MATLAB script to parse the *.sift files
    generated by VisualSFM.

---
+++ matlab/parse_sift.m
@@ -0,0 +1,50 @@
+function [features, descriptors] = parse_sift(sift_filepath)
+
+    % features    = 5   x N, single, (x, y, color, scale, orientation)
+    % descriptors = 128 x N, uint8
+
+    features = [];
+    descriptors = [];
+
+    % Attempt to open the file for reading.
+    file = fopen(sift_filepath, 'rb');
+    if file == -1
+        disp(['ERROR: failed to open file: ' sift_filepath])
+        return
+    end
+
+    % Read the SIFT file header.
+    header = 'SIFTV4.0';
+    data = fread(file, [1, length(header)], '*char');
+    if ~strcmp(header, data)
+        disp('ERROR: expected valid header, received:')
+        disp(data)
+        return
+    end
+
+    % Read the specifications of the file.
+    num_features = fread(file, 1, 'int');
+    num_feature_dims = fread(file, 1, 'int');
+    num_descriptor_dims = fread(file, 1, 'int');
+
+    % Verify the file's specifications.
+    if num_features <= 0
+        disp(['ERROR: expected positive, non-zero number of features: ' num2str(num_features)])
+        return
+    end
+    if num_feature_dims ~= 5
+        disp(['ERROR: expected 5 feature dimensions: ' num2str(num_feature_dims)])
+        return
+    end
+    if num_descriptor_dims ~= 128
+        disp(['ERROR: expected 128 descriptor dimensions: ' num2str(num_descriptor_dims)])
+        return
+    end
+
+    % Read the file's contents.
+    features = fread(file, [5, num_features], '*single');
+    descriptors = fread(file, [128, num_features], '*uint8');
+
+    fclose(file);
+
+end % function
