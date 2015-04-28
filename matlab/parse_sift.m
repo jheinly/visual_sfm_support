8573b7a25ffcccf18e601efbe324704a541b4bba
Jared Heinly <jheinly@users.noreply.github.com>
2015-04-28 09:34:08 -0400

    Verify file footer, and close file on error.
    Verify the existence of the SIFT file footer, and if an error is
    encountered, make sure to close the file before returning from the
    function.

--- matlab/parse_sift.m
+++ matlab/parse_sift.m
@@ -19,6 +19,7 @@ function [keypoints, descriptors] = parse_sift(sift_filepath)
     if ~strcmp(header, data)
         disp('ERROR: expected valid header, received:')
         disp(data)
+        fclose(file);
         return
     end
 
@@ -30,20 +31,34 @@ function [keypoints, descriptors] = parse_sift(sift_filepath)
     % Verify the file's specifications.
     if num_features <= 0
         disp(['ERROR: expected positive, non-zero number of features: ' num2str(num_features)])
+        fclose(file);
         return
     end
     if num_feature_dims ~= 5
         disp(['ERROR: expected 5 feature dimensions: ' num2str(num_feature_dims)])
+        fclose(file);
         return
     end
     if num_descriptor_dims ~= 128
         disp(['ERROR: expected 128 descriptor dimensions: ' num2str(num_descriptor_dims)])
+        fclose(file);
         return
     end
 
     % Read the file's contents.
     keypoints = fread(file, [5, num_features], '*single');
     descriptors = fread(file, [128, num_features], '*uint8');
+    
+    % Read the SIFT file footer.
+    footer = '_EOF';
+    footer(1) = 255;
+    data = fread(file, [1, length(footer)], '*char');
+    if ~strcmp(footer, data)
+        disp('ERROR: expected valid footer, received:')
+        disp(data)
+        fclose(file);
+        return
+    end
 
     % Discard the color information embedded in the 3rd dimension of the
     % keypoints vector.
